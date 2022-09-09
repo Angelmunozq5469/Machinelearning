b3ec9c56f56b83c94846c102ebe5f43d2985f1d4
Angelmunozq5469 <105248544+Angelmunozq5469@users.noreply.github.com>
2022-09-09 12:13:29 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+#Seleccionar una base de datos de imágenes médicas para su
+#procesamiento (ya sea alguna de las facilitadas por el docente, propia o
+#consultada de forma individual).
+#Cogimos una base de datos de la pagina recomendada por el profesor, descargamos un DATASET (2.4 GB) sobre pacientes
+#con problemas de neumania y pulmones normales, lo que se quiere lograr es el aprendizaje de un programa que identifique
+#pacientes afectados o no. 
---
+++ 2.py
@@ -0,0 +1,6 @@
+#Determinar de acuerdo a la información suministrada en la base de
+#datos los objetos de interés a identificar.
+#El dataset contiene imagenes con pulmones afectados con la enfermedad neumonia por otro lado hay imagenes de pulmones
+#que no presentan ninguna enfermedad se espera crear un algoritmo que sea capaz de identificar por si solo cual imagen
+# presentada tiene afeccion y cual no.  Hay carpetas de entrenamiento y testeo. 
+
