ce96200ec6176cebec10783866d012590ae06dfc
Angelmunozq5469 <105248544+Angelmunozq5469@users.noreply.github.com>
2022-09-09 13:30:21 -0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,4 @@
+#DATA BASE DESCARGABLE(ZIP) 
+#https://ucesedu-my.sharepoint.com/:f:/g/personal/munozq_angel_uces_edu_co/EhtqdsDJz3ZNtlgx2Eb0QE8BvxYXK-uYO4-0yTFHw-YZ9Q?e=PqSZbL
+import cv2 as cv
+import numpy as np
